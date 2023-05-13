1ec458f7dfd9b1e5fd709303497c113fc0f9a844
moemenkhaled <moemenkhaled123@yahoo.com>
2023-05-13 17:46:48 +0200

    first

---
+++ admin_login.php
@@ -0,0 +1,102 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+
+   $select_admin = $conn->prepare("SELECT * FROM `admin` WHERE name = ? AND password = ?");
+   $select_admin->execute([$name, $pass]);
+   
+   if($select_admin->rowCount() > 0){
+      $fetch_admin_id = $select_admin->fetch(PDO::FETCH_ASSOC);
+      $_SESSION['admin_id'] = $fetch_admin_id['id'];
+
+      // Check if admin ID is 4
+      if ($_SESSION['admin_id'] === 1) {
+        // Redirect to specific-page.php
+        header('location: admin/dashboard.php');
+      } 
+      // Check if admin ID is 4
+   else if ($_SESSION['admin_id'] === 6) {
+      // Redirect to thirdpage.php
+      header('location: manager/dashboard.php');
+   }
+      else {
+        // Redirect to dashboard.php
+        header('location: chef/dashboard.php');
+      }
+   }else{
+      $message[] = 'incorrect username or password!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+<body>
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<!-- admin login form section starts  -->
+
+<section class="form-container">
+
+   <form action="" method="POST">
+      <h3>login</h3>
+      <div><p>for technical admin login username= technicalAadmin & password = 111</p></div>
+      <div><p>for chef login username= chef & password = 111</p></div>
+      <div><p>for Manager login username= Manager & password =111</p></div>
+      <input type="text" name="name" maxlength="20" required placeholder="enter your username" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="pass" maxlength="20" required placeholder="enter your password" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="submit" value="login now" name="submit" class="btn">
+   </form>
+
+</section>
+
+<!-- admin login form section ends -->
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ chef/dashboard.php
@@ -0,0 +1,80 @@
+<?php
+
+include '../components/connect.php';
+include '../components/chef_header.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+
+<!-- admin dashboard section starts  -->
+
+<section class="dashboard">
+
+   <h1 class="heading">Chef dashboard</h1>
+   <div class="box">
+      <?php
+         $total_pendings = 0;
+         $select_pendings = $conn->prepare("SELECT * FROM `orders` WHERE payment_status = ?");
+         $select_pendings->execute(['pending']);
+         while($fetch_pendings = $select_pendings->fetch(PDO::FETCH_ASSOC)){
+            $total_pendings += $fetch_pendings['total_price'];
+         }
+      ?>
+      <h3><span>$</span><?= $total_pendings; ?><span>/-</span></h3>
+      <p>total pendings</p>
+      <a href="placed_orders.php" class="btn">see orders</a>
+   </div>
+   <div class="box">
+      <?php
+         $select_products = $conn->prepare("SELECT * FROM `products`");
+         $select_products->execute();
+         $numbers_of_products = $select_products->rowCount();
+      ?>
+      <h3><?= $numbers_of_products; ?></h3>
+      <p>products added</p>
+      <a href="products.php" class="btn">see products</a>
+   </div>
+   </div>
+
+</section>
+
+<!-- admin dashboard section ends -->
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
